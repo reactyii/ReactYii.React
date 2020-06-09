64d2a9693b11c48817df03e36b3268369ddbf047
Ondrey <reactyii@mail.ru>
2020-06-09 09:37:21 +0300

    добавлен роутер

--- src/index.tsx
+++ src/index.tsx
@@ -6,13 +6,23 @@ import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 
+import {Page} from './views/Page';
+
+// https://reacttraining.com/react-router/web/guides/quick-start
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Provider store={store}>
+			<Router>
+				<Switch>
+					<Route exact path="/" component={Page}/>
+					<Route path="/app" component={App}/>
+				</Switch>
+			</Router>
+		</Provider>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
