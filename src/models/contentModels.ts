a9c7ec5cb2c03c051e20bd3ee58ccd04af924cfb
Ondrey <reactyii@mail.ru>
2021-03-31 18:27:25 +0300

    начало работы над отображением контента

--- src/models/contentModels.ts
+++ src/models/contentModels.ts
@@ -1,6 +1,17 @@
 import {iTemplate} from './templateModels';
 export interface iContent {
+    id: string;
+    name: string;
+    priority: number;
+    parent_id: number | null;
+    path: string;
+    content: string;
+    type: string;
+    childs: iContent[] | undefined;
 	template_keys: string[];
 	template: iTemplate;
-	childs: iContent[];
+}
+
+export interface iContentProps {
+    content: iContent;
 }
---
+++ src/views/Content.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { iContentProps } from '../models/contentModels';
+
+export class Content extends React.Component<iContentProps, {}> {
+
+	render() {
+
+		return (
+			<div>
+				
+				
+			</div>
+		);
+	}
+}
