bb2eabb76689d021367c86fc74785a169e06a24a
Ondrey <reactyii@mail.ru>
2021-04-02 18:22:43 +0300

    отображение контента

--- src/views/Content.tsx
+++ src/views/Content.tsx
@@ -10,7 +10,20 @@ export class Content extends React.Component<iContentProps, {}> {
 		return this.props.content.map(item => {
 			Console.log(item);
 			if (typeof item.childs !== 'undefined' && item.childs.length > 0) return <Content key={item.id} content={item.childs} />;
+
+			// здесь контент как html
 			if (item.type == null) return <Html html={item.content} />;//item.content;
+			if (item.type == 'string') return item.content;
+			// допилить другие примитивы ...
+
+			// контент с шаблоном
+			if (item.template) {
+				if (typeof item.template.filename !== 'undefined') {
+
+				}
+				//return <Html html={item.template.template} />;
+			}
+
 		});
 	}
 }
