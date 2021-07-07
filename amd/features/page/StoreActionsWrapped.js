a4e242a511c10fe3161795554812be7ab8e69a2d
Ondrey <reactyii@mail.ru>
2021-07-07 12:25:05 +0300

    удаляем первую поппытку сбора либы

--- amd/features/page/StoreActionsWrapped.js
+++
@@ -1,8 +0,0 @@
-define(["require", "exports", "react-redux", "./StoreActions"], function (require, exports, react_redux_1, StoreActions_1) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    exports.default = react_redux_1.connect(StoreActions_1.mapStateToProps, StoreActions_1.mapDispatchToProps, //dispatchProps
-    null, {
-        forwardRef: true,
-    })(StoreActions_1.StoreActions);
-});
--- amd/setupTests.js
+++
@@ -1,4 +0,0 @@
-define(["require", "exports", "@testing-library/jest-dom/extend-expect"], function (require, exports) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-});
