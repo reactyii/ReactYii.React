956085a09e475e27c50941e830fdf196a305277e
Ondrey <reactyii@mail.ru>
2021-07-07 12:25:23 +0300

    удаляем первую поппытку сбора либы

--- es/app/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
-import pageReducer from '../features/page/pageSlice';
-export var store = configureStore({
-    reducer: {
-        counter: counterReducer,
-        page: pageReducer,
-    },
-});
--- es/features/page/StoreActionsWrapped.js
+++
@@ -1,6 +0,0 @@
-import { connect } from 'react-redux';
-import { mapDispatchToProps, mapStateToProps, StoreActions } from './StoreActions';
-export default connect(mapStateToProps, mapDispatchToProps, //dispatchProps
-null, {
-    forwardRef: true,
-})(StoreActions);
