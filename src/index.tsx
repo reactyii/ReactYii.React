d6ee7e693d03f8b4480c23798a502e60b5f1016a
Ondrey <reactyii@mail.ru>
2021-07-08 09:44:03 +0300

    сделаем реэкспорт

--- src/index.tsx
+++ src/index.tsx
@@ -9,8 +9,10 @@ import { Console } from './models/commonModels';
 import { Utils } from './helpers/Utils';
 import { Templates } from './views/templates';
 import { App } from './App';
+import { Html } from './views/Html';
+import { Content } from './views/Content';
 
-export { App, Utils, Console };
+export { App, Utils, Console, Templates, Html, Content };
 
 Console.log('-->', process.env.NODE_ENV);
 //Console.log('-->', process.env.REACT_APP_HOST);
