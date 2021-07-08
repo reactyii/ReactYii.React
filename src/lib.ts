ffb9dc10d0c719b915e86d96770a6a7fdd7191c5
Ondrey <reactyii@mail.ru>
2021-07-08 17:36:04 +0300

    добавим интерфейс iContentProps в либу

--- src/lib.ts
+++ src/lib.ts
@@ -4,5 +4,6 @@ import { Templates } from './views/templates';
 import { App } from './App';
 import { Html } from './views/Html';
 import { Content } from './views/Content';
+import { iContentProps } from './models/contentModels';
 
-export { App, Utils, Console, Templates, Html, Content };
+export { App, Utils, Console, Templates, Html, Content, iContentProps };
